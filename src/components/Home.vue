75ef21f99f41875d8b77e14ad3c457b29118acd5
邢真杰 <1367261130@qq.com>
2022-02-24 17:07:09 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,151 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+  <el-container class="home_container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="@/assets/logo.jpeg" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ?'64px':'200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :router="true"
+          :default-active="activePath"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id+''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+             :index="'/'+subItem.path" 
+             v-for="subItem in item.children" :key="subItem.id"
+             @click="saveNavState('/'+subItem.path)"
+            >
+              <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-menu"></i>
+              <!-- 文本 -->
+              <span>{{subItem.authName}}</span>
+            </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    props: {
-
+  props: {},
+  data() {
+    return {
+      // 左侧菜单数据
+      menulist:[],
+      iconsObj:{
+        '125':'el-icon-user-solid',
+        '103':'el-icon-box',
+        '101':'el-icon-s-goods',
+        '102':'el-icon-receiving',
+        '145':'el-icon-data-line'
+      },
+      // 是否折叠
+      isCollapse:false,
+      // 被激活的链接地址
+      activePath:''
+    };
+  },
+  created () {
+    this.getMenuList(),
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
     },
-    data() {
-        return {
-
-        };
+    // 获取所有菜单
+    async getMenuList() {
+      const {data:res} = await this.$http.get('menus')
+      if(res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res);
     },
-    methods: {
-        logout() {
-            window.sessionStorage.clear()
-            this.$router.push('/login')
-        }
+    // 点击按钮切换菜单的折叠与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     },
-    components: {
-
-    },
+    // 保存链接的激活状态
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath',activePath)
+      this.activePath = activePath
+    }
+  },
+  components: {},
 };
 </script>
 
 <style scoped lang="less">
-
+.home_container {
+  height: 100%;
+}
+.el-header {
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #333744;
+  font-size: 20px;
+  div {
+    text-align: center;
+    width: 80%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    img {
+      height: 55px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: rgb(248, 248, 248);
+}
+.toggle-button {
+  background-color: #4A5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: .2em;
+  cursor: pointer;
+}
+.el-menu {
+  border-right: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,22 @@ import './plugins/element.js'
 // 导入全局样式表
 import '@/assets/css/global.css'
 
+// 引入element插件
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+
+Vue.use(ElementUI, { locale })
+
 // 导入axios
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
